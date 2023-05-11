5a0789b6263d569829fb4f0524ae881147267d08
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-05-11 05:45:12 -0700

    add data entities

---
+++ entity.py
@@ -0,0 +1,33 @@
+import uuid
+from pydantic import BaseModel, Field
+
+
+class ScenarioParameters(BaseModel):
+    random_word: str
+    scope: str
+    severity: str
+    region: str
+    category: str
+    domain: str
+
+
+class ScenarioPrompt(BaseModel):
+    system_message: str
+    parameters: ScenarioParameters
+
+
+class Scenario(BaseModel):
+    prompt: ScenarioPrompt
+    description: str
+    id: uuid.UUID = Field(default_factory=uuid.uuid4)
+
+
+class ActionPrompt(BaseModel):
+    system_message: str
+    scenario: Scenario
+
+
+class Action(BaseModel):
+    prompt: ActionPrompt
+    description: str
+    id: uuid.UUID = Field(default_factory=uuid.uuid4)
