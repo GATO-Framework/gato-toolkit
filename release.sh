1f4fccbd5d800d82c48139165de31fceae87f2aa
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-05-12 00:19:11 -0700

    add release script

---
+++ release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Remove previous distributions
+rm -rf dist
+
+# Build distributions
+python setup.py sdist bdist_wheel
+
+# Upload distributions to PyPI
+twine upload dist/*
+
+# Clean up
+rm -rf build dist *.egg-info
+
+echo "Release completed successfully."
